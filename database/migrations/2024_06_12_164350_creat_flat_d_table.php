bdd92c4287c858a3c28a77f00dd3e510a0c98200
patramanik <patramanik409@gmail.com>
2024-06-13 07:44:54 +0530

    first commit

---
+++ database/migrations/2024_06_12_164350_creat_flat_d_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flatD', function (Blueprint $table) {
+            $table->id();
+            $table->string('flat_no')->unique();
+            $table->string('owner_name');
+            $table->float('monthly_rate')->default(1270.00);
+            $table->float('maintenance_charges')->default(1000.00);
+            $table->float('collection_for_major_maintenance')->default(270.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flatD');
+    }
+};
---
+++ database/migrations/2024_06_12_174128_creat_flat_a_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flatA', function (Blueprint $table) {
+            $table->id();
+            $table->string('flat_no')->unique();
+            $table->string('owner_name');
+            $table->float('monthly_rate')->default(1240.00);
+            $table->float('maintenance_charges')->default(1000.00);
+            $table->float('collection_for_major_maintenance')->default(240.00);
+            $table->timestamps();
+        });
+
+        // Schema::create('flatA', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('flat_no')->primary();
+        //     $table->string('owner_name');
+        //     $table->float('monthly_rate')->default(1240.00);
+        //     $table->float('monthly_maintenance_charges')->default(1000.00);
+        //     $table->float('monthly_collection_for_major_maintenance')->default(1000.00);
+        //     $table->timestamps();
+        // });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flatA');
+    }
+};
